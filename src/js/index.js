50976539662d12d4f320cbff7e2aaf85376e47bd
Leonardo Almir Stynner <leonardostynner@live.com>
2023-10-10 21:25:56 -0300

    Concluiindo javaScript

--- src/js/index.js
+++ src/js/index.js
@@ -12,19 +12,24 @@
         passo 3 - verificar se já exista um personagem selecionado, se sim, devemos remover a seleção dele.
 */
 
-// OBJETIVO 1 - quando clicar no botão do personagem na lista, marcar o botao como selecionado.
-// passo 1 - pegar os botões no JS pra poder verificar quando o usuário clicar em cima de um deles.
 const botoes = document.querySelectorAll(".botao");
+const personagens = document.querySelectorAll(".personagem");
 
-botoes.forEach(botao => {
+botoes.forEach((botao, indice) => {
     botao.addEventListener("click", () => {
-        // passo 3 - verificar se já existe um botão selecionado, se sim, devemos remover a seleção dele.
-        const botaoSelecionado = document.querySelector(".botao.selecionado");
-        botaoSelecionado.classList.remove("selecionado");
-
-        // passo 2 - adicionar a classe "selecionado" no botão que o usuário clicou.
+        desselecionarBotao();
+        desselecionarPersonagem();
         botao.classList.add("selecionado");
+        personagens[indice].classList.add("selecionado");
     })
 });
 
+function desselecionarPersonagem() {
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+}
 
+function desselecionarBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
